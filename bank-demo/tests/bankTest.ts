f6f5842617d9d33a3c92d7c31f9cea7bddb19db0
Sydney Bao <bao.sy@northeastern.edu>
2024-09-13 19:11:53 -0400

    resolves #2

--- bank-demo/tests/bankTest.ts
+++ bank-demo/tests/bankTest.ts
@@ -3,8 +3,16 @@ import Bank from "../src/bank";
 const bank = new Bank();
 
 const account = bank.createAccount("John Doe", 29, "2938298");
+const account2 = bank.createAccount("Jane Doe", 29, "12345");
 
+//create account tests
 if (account.accountNumber == "2938298") {
+    console.log("Scenario 1 pass")
+} else {
+    console.log("Scenario 1 fail")
+}
+
+if (account2.accountNumber == "12345") {
     console.log("Scenario 1 pass")
 } else {
     console.log("Scenario 1 fail")
@@ -13,6 +21,28 @@ if (account.accountNumber == "2938298") {
 try {
     bank.createAccount("John Doe", 29, "2938298");
     console.log("Scenario 2 fail")
-} catch (e) {
+} catch (_) {
     console.log("Scenario 2 pass")
+}
+
+//deposit tests
+bank.deposit("John Doe", "2938298", 10);
+if (account.balance == 10) {
+    console.log("Scenario 1 pass")
+} else {
+    console.log("Scenario 1 fail")
+}
+
+try {
+    bank.deposit("John Doe", "12345", 10);
+    console.log("Scenario 2 fail")
+} catch (_) {
+    console.log("Scenario 2 pass")
+}
+
+try {
+    bank.deposit("John Doe", "10", 10);
+    console.log("Scenario 3 fail")
+} catch (_) {
+    console.log("Scenario 3 pass")
 }
